9d8c7c61309fc8b769273da7833c45411c7ca0d2
Borcanac97 <79090706+Borcanac97@users.noreply.github.com>
2022-11-09 09:35:18 +0100

    ToDo.Web-Startup

--- PD.Workademy.ToDo/src/Web/PD.Workademy.ToDo.Web/Program.cs
+++ PD.Workademy.ToDo/src/Web/PD.Workademy.ToDo.Web/Program.cs
@@ -1,37 +1,24 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using PD.Workademy.ToDo.Application.IServices;
 using PD.Workademy.ToDo.Application.Services;
 using PD.Workademy.ToDo.Domain.SharedKarnel.Interfaces.Repository;
 using PD.Workademy.ToDo.Infrastructure.Persistance;
 using PD.Workademy.ToDo.Infrastructure.Persistance.Repositories;
+using PD.Workademy.ToDo.Web;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<ApplicationDBContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Todo"));
-});
-
-
-builder.Services.AddTransient<ICategoryService, CategoryService>();
-builder.Services.AddTransient<IUserService, UserService>();
-builder.Services.AddTransient<IToDoItemService, ToDoItemService>();
-
-
-builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
-builder.Services.AddTransient<IUserRepository, UserRepository>();
-builder.Services.AddTransient<IToDoItemRepository, ToDoItemRepository>();
-
-
+var builder = WebApplication.CreateBuilder(args);
+var startup = new Startup(builder.Configuration);
+startup.ConfigureService(builder.Services);
 
 var app = builder.Build();
+startup.Configure(app);
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ PD.Workademy.ToDo/src/Web/PD.Workademy.ToDo.Web/Startup.cs
@@ -0,0 +1,60 @@
+﻿
+using PD.Workademy.ToDo.Application.IServices;
+using PD.Workademy.ToDo.Application.Services;
+using PD.Workademy.ToDo.Domain.SharedKarnel.Interfaces.Repository;
+using PD.Workademy.ToDo.Infrastructure.Persistance.Repositories;
+using PD.Workademy.ToDo.Infrastructure.Persistance;
+using Serilog;
+using Microsoft.EntityFrameworkCore;
+
+namespace PD.Workademy.ToDo.Web
+{
+    public class Startup
+    {
+        public Startup(IConfigurationRoot configurationRoot)
+        {
+            Configuration = configurationRoot;
+        }
+        public IConfigurationRoot Configuration { get;}
+
+        public void ConfigureService(IServiceCollection service)
+        {
+            service.AddControllers();
+            service.AddEndpointsApiExplorer();
+            service.AddSwaggerGen();
+
+            service.AddDbContext<ApplicationDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("Todo"));
+            });
+
+
+            service.AddTransient<ICategoryService, CategoryService>();
+            service.AddTransient<IUserService, UserService>();
+            service.AddTransient<IToDoItemService, ToDoItemService>();
+
+
+            service.AddTransient<ICategoryRepository, CategoryRepository>();
+            service.AddTransient<IUserRepository, UserRepository>();
+            service.AddTransient<IToDoItemRepository, ToDoItemRepository>();
+
+            static IHostBuilder CreateHostBuilder(string[] args) =>
+                       Host.CreateDefaultBuilder(args)
+                           .UseWindowsService()
+                           .UseSerilog((context, config) =>
+                           {
+                               config.ReadFrom.Configuration(context.Configuration);
+                           });
+
+        }
+
+        public void Configure(IApplicationBuilder application)
+        {
+            application.UseRouting();
+
+            application.UseEndpoints(x=>x.MapControllers());
+        }
+    
+    
+    }
+}

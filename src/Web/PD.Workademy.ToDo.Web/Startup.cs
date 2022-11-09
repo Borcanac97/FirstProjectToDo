e3da7288ced0e620f83fae3459358c80c5c7637f
Borcanac97 <79090706+Borcanac97@users.noreply.github.com>
2022-11-09 10:02:08 +0100

    finish-Startup

---
+++ src/Web/PD.Workademy.ToDo.Web/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PD.Workademy.ToDo.Application.IServices;
+using PD.Workademy.ToDo.Application.Services;
+using PD.Workademy.ToDo.Domain.SharedKarnel.Interfaces.Repository;
+using PD.Workademy.ToDo.Infrastructure.Persistance;
+using PD.Workademy.ToDo.Infrastructure.Persistance.Repositories;
+
+namespace PD.Workademy.ToDo.Web
+{
+    public class Startup
+    {
+        public Startup(IConfigurationRoot configurationRoot)
+        {
+            Configuration = configurationRoot;
+        }
+        public IConfigurationRoot Configuration { get; }
+        public void ConfigureService(IServiceCollection service)
+        {
+            service.AddControllers();
+            service.AddEndpointsApiExplorer();
+            service.AddSwaggerGen();
+
+            service.AddDbContext<ApplicationDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("Todo"));
+            });
+
+            service.AddTransient<ICategoryService, CategoryService>();
+            service.AddTransient<IUserService, UserService>();
+            service.AddTransient<IToDoItemService, ToDoItemService>();
+            service.AddTransient<ICategoryRepository, CategoryRepository>();
+            service.AddTransient<IUserRepository, UserRepository>();
+            service.AddTransient<IToDoItemRepository, ToDoItemRepository>();
+        }
+        public void Configure(IApplicationBuilder application)
+        {
+            application.UseRouting();
+            application.UseEndpoints(x => x.MapControllers());
+        }
+    }
+}

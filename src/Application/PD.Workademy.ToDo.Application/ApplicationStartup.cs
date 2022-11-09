8ac7ab5c888605af6a94e620dcbf9129807cea4b
Borcanac97 <79090706+Borcanac97@users.noreply.github.com>
2022-11-09 11:39:57 +0100

    ApplicationStartup

---
+++ src/Application/PD.Workademy.ToDo.Application/ApplicationStartup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PD.Workademy.ToDo.Application.IServices;
+using PD.Workademy.ToDo.Application.Services;
+
+namespace PD.Workademy.ToDo.Application
+{
+    public class ApplicationStartup
+    {
+        public ApplicationStartup(IConfigurationRoot configurationRoot)
+        {
+            Configuration = configurationRoot;
+        }
+        public IConfigurationRoot Configuration { get; }
+        public void ConfigureService(IServiceCollection service)
+        {
+            service.AddTransient<ICategoryService, CategoryService>();
+            service.AddTransient<IUserService, UserService>();
+            service.AddTransient<IToDoItemService, ToDoItemService>();
+        }
+    }
+}
